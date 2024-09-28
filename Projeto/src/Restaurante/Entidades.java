2b0cd48c5b1de38d27b4a24c974bc69ad5e4bbbd
Pedro-Vic3005 <132630207+Pedro-Vic3005@users.noreply.github.com>
2024-09-28 19:58:17 -0300

    NEW ARQUIVO

---
+++ Projeto/src/Restaurante/Entidades.java
@@ -0,0 +1,90 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Pedido {
+    private String cliente;
+    private String comanda;
+
+    public Pedido(String cliente, String comanda) {
+        this.cliente = cliente;
+        this.comanda = comanda;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public String getComandas() {
+        return comanda;
+    }
+}
+
+class Itempedido {
+    private String prato;
+    private String naturalidade;
+    private int vezespedidos;
+    private List<Integer> avaliacoes;
+
+    public Itempedido(String prato, String naturalidade, int vezespedidos, List<Integer> avaliacoes) {
+        this.prato = prato;
+        this.naturalidade = naturalidade;
+        this.vezespedidos = vezespedidos;
+        this.avaliacoes = avaliacoes;
+    }
+
+    public String getPrato() {
+        return prato;
+    }
+
+    public String getNaturalidade() {
+        return naturalidade;
+    }
+
+    public void incrementarvezespedidos() {
+        this.vezespedidos++;
+    }
+
+    public int getVezesPedidos() {
+        return vezespedidos;
+    }
+
+    public void adicionarAvaliacao(int estrelas) {
+        avaliacoes.add(estrelas);
+    }
+
+    public double calcularMediaAvaliacoes() {
+        if (avaliacoes.isEmpty()) return 0;
+        int soma = 0;
+        for (int nota : avaliacoes) {
+            soma += nota;
+        }
+        return (double) soma / avaliacoes.size();
+    }
+}
+
+class Mesa {
+    private Pedido pedido;
+    private Itempedido itempedido;
+    private int numeromesa;
+
+    public Mesa(Pedido pedido, Itempedido itempedido, int numeromesa) {
+        this.pedido = pedido;
+        this.itempedido = itempedido;
+        this.numeromesa = numeromesa;
+        itempedido.incrementarvezespedidos();
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public Itempedido getitempedido() {
+        return itempedido;
+    }
+
+    @Override
+    public String toString() {
+        return "Prato " + itempedido.getPrato() + " para o cliente " + pedido.getCliente() + " com a comanda "+ pedido.getComandas()+ " na mesa " + numeromesa;
+    }
+}
+
