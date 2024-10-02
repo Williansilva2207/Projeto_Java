81084b5c4c369b85f8e230b589cc27c416d85417
Willian Rodrigues <149281430+Williansilva2207@users.noreply.github.com>
2024-10-02 14:04:48 +0000

    fazerPedidos

--- Projeto/src/Restaurante/RestauranteManager.java
+++ Projeto/src/Restaurante/RestauranteManager.java
@@ -20,25 +20,14 @@ class RestauranteManager {
         mesas.add(new Mesa(4));
         mesas.add(new Mesa(5));
 
-        cardapio.comida.add("Tacacá\n");
-        cardapio.descrision.add("Uma sopa típica feita com tucupi (caldo fermentado da mandioca),\ngoma de mandioca, camarão e jambu (erva que provoca leve dormência na boca).\nMuito popular no Pará e Amazonas.\n");
-        cardapio.nationalite.add("Região Norte\n");
-        cardapio.comida.add("Acarajé\n");
-        cardapio.descrision.add("Um bolinho de feijão-fradinho frito no azeite de dendê,\nrecheado com vatapá, camarão seco e caruru, típico da Bahia, principalmente em Salvador.\n");
-        cardapio.nationalite.add("Região Nordeste\n");
-        cardapio.comida.add("Arroz com Pequi\n");
-        cardapio.descrision.add("Um prato de arroz cozido com o pequi, um fruto típico do Cerrado, muito apreciado em Goiás e Mato Grosso.\n");
-        cardapio.nationalite.add("Região Centro-Oeste\n");
-        cardapio.comida.add("Feijão Tropeiro\n");
-        cardapio.descrision.add("Um prato feito com feijão, farinha de mandioca, linguiça, bacon e ovos,\ntípico de Minas Gerais, mas popular em toda a região Sudeste.\n");
-        cardapio.nationalite.add("Região Sudeste\n");
-        cardapio.comida.add("Barreado\n");
-        cardapio.descrision.add("Um prato tradicional do litoral do Paraná. Ele é feito com carne bovina cozida\nlentamente em uma panela de barro, junto com temperos como alho, cebola, louro e cominho.\n A carne é cozida até se desmanchar e é servida com farinha de mandioca e banana.\n É um prato festivo e muito apreciado na região.\n");
-        cardapio.nationalite.add("Região Sul\n");
-        cardapio.comida.add("Pato no Tucupi\n");
-        cardapio.descrision.add("Um prato típico do Pará, onde o pato é cozido com tucupi e jambu, e geralmente servido em ocasiões especiais.\n");
-        cardapio.nationalite.add("Região Norte\n");
+        cardapio.add(new Cardapio("Tacacá", "Uma sopa típica feita com tucupi, goma de mandioca, camarão e jambu.", "Região Norte"));
+        cardapio.add(new Cardapio("Acarajé", "Um bolinho de feijão-fradinho frito no azeite de dendê, recheado com vatapá, camarão seco e caruru.", "Região Nordeste"));
+        cardapio.add(new Cardapio("Arroz com Pequi", "Um prato de arroz cozido com o pequi, um fruto típico do Cerrado.", "Região Centro-Oeste"));
+        cardapio.add(new Cardapio("Feijão Tropeiro", "Um prato feito com feijão, farinha de mandioca, linguiça, bacon e ovos.", "Região Sudeste"));
+        cardapio.add(new Cardapio("Barreado", "Um prato tradicional do litoral do Paraná feito com carne bovina cozida lentamente.", "Região Sul"));
+        cardapio.add(new Cardapio("Pato no Tucupi", "Um prato típico do Pará, onde o pato é cozido com tucupi e jambu.", "Região Norte"));
     }
+    
 
     public void mostrarDisponibilidadeMesa(){
         for(Mesa i : mesas){
@@ -55,14 +44,15 @@ class RestauranteManager {
         for(Cardapio i : cardapio){
             ctd ++;
             System.out.printf("____________________ Digite %d ________________________", ctd);
-            System.out.println("Comida: " + i.getComida);
-            System.out.println("Descrição: " + i.getDescrision);
+            System.out.println("Comida: " + i.getComida());
+            System.out.println("Descrição: " + i.getDescrision());
+            System.out.println("Nacionalidade: " + i.getNationalite());
             System.out.println("____________________________________________________________");
         }
 
         System.out.println("Digite o número do alimento que deseja:");
-        opcao = sc.Int();
-        itens.pratos.add(cardapio.comida.get(opcao));
+        opcao = sc.nextInt();
+        itens.add(cardapio.get(opcao-1));
     }
 
 }
