1edd7cdbde201a0041f1e09f2eb2c16854e79fe2
Willian Rodrigues <149281430+Williansilva2207@users.noreply.github.com>
2024-10-02 13:30:30 +0000

    Att Cardapio

--- Projeto/src/Restaurante/RestauranteManager.java
+++ Projeto/src/Restaurante/RestauranteManager.java
@@ -19,7 +19,19 @@ class RestauranteManager {
         mesas.add(new Mesa(3));
         mesas.add(new Mesa(4));
         mesas.add(new Mesa(5));
-        
+
+        cardapio.comida.add("Tacacá\n");
+        cardapio.descrision.add("Uma sopa típica feita com tucupi (caldo fermentado da mandioca),\ngoma de mandioca, camarão e jambu (erva que provoca leve dormência na boca).\nMuito popular no Pará e Amazonas.\n");
+        cardapio.comida.add("Acarajé\n");
+        cardapio.descrision.add("Um bolinho de feijão-fradinho frito no azeite de dendê,\nrecheado com vatapá, camarão seco e caruru, típico da Bahia, principalmente em Salvador.\n");
+        cardapio.comida.add("Arroz com Pequi\n");
+        cardapio.descrision.add("Um prato de arroz cozido com o pequi, um fruto típico do Cerrado, muito apreciado em Goiás e Mato Grosso.\n");
+        cardapio.comida.add("Feijão Tropeiro\n");
+        cardapio.descrision.add("Um prato feito com feijão, farinha de mandioca, linguiça, bacon e ovos,\ntípico de Minas Gerais, mas popular em toda a região Sudeste.\n");
+        cardapio.comida.add("Barreado\n");
+        cardapio.descrision.add("Um prato tradicional do litoral do Paraná. Ele é feito com carne bovina cozida\nlentamente em uma panela de barro, junto com temperos como alho, cebola, louro e cominho.\n A carne é cozida até se desmanchar e é servida com farinha de mandioca e banana.\n É um prato festivo e muito apreciado na região.\n");
+        cardapio.comida.add("Pato no Tucupi\n");
+        cardapio.descrision.add("Um prato típico do Pará, onde o pato é cozido com tucupi e jambu, e geralmente servido em ocasiões especiais.\n");
         
     }
 
