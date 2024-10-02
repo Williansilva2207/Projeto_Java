2bc12cbe990ac0a7716645531c87f7a62b2b3f3d
Willian Rodrigues <149281430+Williansilva2207@users.noreply.github.com>
2024-10-02 13:00:33 +0000

    Att RestauranteManager

--- Projeto/src/Restaurante/RestauranteManager.java
+++ Projeto/src/Restaurante/RestauranteManager.java
@@ -11,7 +11,7 @@ class RestauranteManager {
         this.pedidos = new ArrayList<>();
         this.mesas = new ArrayList<>();
         this.itens = new ArrayList<>();
-        
+
         mesas.add(new Mesa(1));
         mesas.add(new Mesa(2));
         mesas.add(new Mesa(3));
@@ -21,10 +21,11 @@ class RestauranteManager {
         
     }
 
-    public mostrarDisponibilidadeMesa(){
+    public void mostrarDisponibilidadeMesa(){
         for(Mesa i : mesas){
             if(i.isDisponibilidade()){
                 System.out.println("Mesa " + i.getNumeroMesa() + "Disponível");
             }
         }
     }
+}
